3056f1d2c73fba3c744567a078813743374cec15
john carbajal <jcarbajal@solintels.com>
2024-10-28 11:48:28 -0500

    feat: hamburger menu created successfully

---
+++ src/components/Menu/hambuger.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useState } from "react";
+import "./hamburger.css";
+import Hamburger from "hamburger-react";
+import { Link } from "react-router-dom";
+import { FaChalkboard, FaComments } from "react-icons/fa";
+
+const HamburgerComponent: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <div>
+      <Hamburger size={24} toggled={open} toggle={setOpen} />
+      {open && (
+        <div className="OpenMenu">
+          <header className="OpenMenu__header"></header>
+          <main className="OpenMenu__header__main">
+            <nav>
+              <ul>
+                <li>
+                  <Link to={"dashboard"}>
+                    {" "}
+                    <FaChalkboard /> dashboard
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"chat"}>
+                    <FaComments /> chat
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </main>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HamburgerComponent;
